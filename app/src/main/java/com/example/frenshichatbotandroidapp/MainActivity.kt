e69a6c4a587215f606c146f08df7a2ab1616f7c4
Naima Zouidi <naimazouidi2@gmail.com>
2024-03-24 13:40:37 +0100

    Create GUI

---
+++ app/src/main/java/com/example/frenshichatbotandroidapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.frenshichatbotandroidapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
