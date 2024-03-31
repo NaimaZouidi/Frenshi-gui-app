597237c7c9995e7cb129216b1e380031bb7a46b5
Naima Zouidi <naimazouidi2@gmail.com>
2024-03-31 14:06:32 +0200

    Improve GUI with custom messages layouts

--- app/src/main/java/com/example/frenshichatbotandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/frenshichatbotandroidapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.frenshichatbotandroidapp
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +15,14 @@ class MainActivity : ComponentActivity() {
         var chat = ""
         var user = UserInput()
         val frenshiChatbotEN = FrenshiEN()
-        var chattingMsgs = findViewById<TextView>(R.id.chat)
+        var chattingMsgs = findViewById<RecyclerView>(R.id.chat)
         val tags = listOf("GreetingDayEN", "GreetingEveningEN", "GoodbyeDayEN", "GoodByeEveningEN")
         val sendButton = findViewById<Button>(R.id.sendButton)
         //set user input and predict answer everytime the send button is clicked
         sendButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?)
             {
-                user.setUserInput(findViewById<TextView>(R.id.userInput).text)
+                //user.setUserInput(findViewById<TextView>(R.id.userInput).text)
                 var index = (1..3).random()
                 chat += user.getUserInput()
                 chat += "\n"
@@ -29,8 +31,9 @@ class MainActivity : ComponentActivity() {
                 frenshiChatbotEN.setAnswer(ChatBotanswer)
                 chat += ChatBotanswer
                 chat += "\n"
-                chattingMsgs.text = chat
+                //chattingMsgs.
             }
         })
+
     }
 }
